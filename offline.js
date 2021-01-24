2374a13115aceda9b64ce9f6eec23e444f3e0343
guruchian <30399464+semicon@users.noreply.github.com>
2021-01-24 19:00:41 +0700

    quizzx

---
+++ offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1611487702,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bgquiz-sheet0.png",
+		"images/quiz-sheet0.png",
+		"images/btnplay-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/popup-sheet0.png",
+		"images/close-sheet0.png",
+		"images/hud-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
